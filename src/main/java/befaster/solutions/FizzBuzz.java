171154d28ad0fc88c01b0f5f6a7686ece08e5889
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-25 10:54:13 +0000

    Sat Nov 25 10:54:13 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -2,16 +2,30 @@ package befaster.solutions;
 
 public class FizzBuzz {
 
+	public static boolean singleDigitNumber(String number){
+		if (number.length()==1)
+			return true;
+		char c = number.charAt(0);
+		for(int i = 1;i<number.length();i++){
+			if (number.charAt(i)!=c)
+				return false;
+		}
+		return true;
+	}
+
 	public static String fizzBuzz(Integer number) {
 		String numStr = String.valueOf(number);
+		StringBuilder sb = new StringBuilder();
 		if ((number % 5 == 0 || numStr.contains("5"))
 				&& (number % 3 == 0 || numStr.contains("3")))
-			return "fizz buzz";
+			sb.append("fizz buzz");
 		if (number % 5 == 0 || numStr.contains("5"))
-			return "buzz";
+			sb.append("buzz");
 		if (number % 3 == 0 || numStr.contains("3"))
-			return "fizz";
-		return numStr;
+			sb.append("fizz");
+		if (number>10 && singleDigitNumber(numStr))
+			sb.append(" deluxe");
+		return sb.toString();
 	}
 
 }
